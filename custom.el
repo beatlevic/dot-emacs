9fa57446db561ef4ee73e236244b8f79d58f3d42
beatle <coen@beatletech.com>
2011-07-26 16:40:05 +0200

    Added custom.el to version control

---
+++ custom.el
@@ -0,0 +1,28 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(speedbar-show-unknown-files t)
+ '(speedbar-supported-extension-expressions (quote (".org" ".[ch]\\(\\+\\+\\|pp\\|c\\|h\\|xx\\)?" ".tex\\(i\\(nfo\\)?\\)?" ".el" ".emacs" ".l" ".lsp" ".p" ".java" ".js" ".f\\(90\\|77\\|or\\)?" ".ada" ".p[lm]" ".tcl" ".m" ".scm" ".pm" ".py" ".g" ".s?html" ".ma?k" "[Mm]akefile\\(\\.in\\)?" ".clj" ".rb" ".erb" ".php")))
+ '(speedbar-use-images nil)
+ '(speedbar-verbosity-level 1)
+ '(sr-speedbar-auto-refresh nil)
+ '(sr-speedbar-delete-windows nil)
+ '(sr-speedbar-right-side nil)
+ '(sr-speedbar-skip-other-window-p t)
+ '(sr-speedbar-width-x 36))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(ecb-default-highlight-face ((((class color) (background dark)) (:background "blue"))))
+ '(ecb-directory-face ((((class color) (background dark)) (:inherit ecb-default-highlight-face :background "blue"))))
+ '(linum ((t (:inherit default :foreground "#6c6c6c"))))
+ '(org-level-1 ((t (:inherit nil :foreground "LightSkyBlue"))))
+ '(org-level-2 ((t (:foreground "#94bff3"))))
+ '(org-level-3 ((t (:foreground "#8DA6CE"))))
+ '(speedbar-button-face ((((class color) (background dark)) (:foreground "darkgrey"))))
+ '(speedbar-file-face ((((class color) (background dark)) (:foreground "LightSkyBlue"))))
+ '(speedbar-selected-face ((((class color) (background dark)) (:foreground "cyan" :underline t)))))
