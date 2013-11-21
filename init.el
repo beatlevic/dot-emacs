0374eaee795f0964777902987469a179b178a3e6
Coen Stevens <coen@beatletech.com>
2013-11-21 13:17:06 +0100

    Added rename file and move to new directory (C-x C-r AND C-x C-m)

--- init.el
+++ init.el
@@ -196,6 +196,9 @@
 
 (global-set-key (kbd "S-<f5>") 'revert-all-buffers) ;; M-x revert-buffer => reads buffer from file again
 
+(global-set-key (kbd "C-x C-r") 'rename-file-and-buffer);
+(global-set-key (kbd "C-x C-m") 'move-buffer-file);
+
 ;; Activate occur easily inside isearch
 (define-key isearch-mode-map (kbd "C-o")
   (lambda () (interactive)
@@ -352,3 +355,52 @@
                    (let ((matching (matching-paren delimiter)))
                      (and matching (char-syntax matching)))))))
 
+;; source: http://steve.yegge.googlepages.com/my-dot-emacs-file
+(defun rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (message "A buffer named '%s' already exists!" new-name)
+        (progn
+          (rename-file name new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
+
+(defun move-buffer-file (dir)
+  "Moves both current buffer and file it's visiting to DIR." (interactive "DNew directory: ")
+  (let* ((name (buffer-name))
+	 (filename (buffer-file-name))
+	 (dir
+          (if (string-match dir "\\(?:/\\|\\\\)$")
+              (substring dir 0 -1) dir))
+	 (newname (concat dir "/" name)))
+
+    (if (not filename)
+	(message "Buffer '%s' is not visiting a file!" name)
+      (progn
+        (copy-file filename newname 1)
+	(delete-file filename)
+        (set-visited-file-name newname)
+        (set-buffer-modified-p nil)
+	t))))
+
+(defun swap-windows ()
+ "If you have 2 windows, it swaps them."
+(interactive)
+(cond ((not (= (count-windows) 2)) (message "You need exactly 2 windows to do this."))
+ (t
+  (let* ((w1 (first (window-list)))
+	 (w2 (second (window-list)))
+	 (b1 (window-buffer w1))
+	 (b2 (window-buffer w2))
+	 (s1 (window-start w1))
+	 (s2 (window-start w2)))
+    (set-window-buffer w1 b2)
+    (set-window-buffer w2 b1)
+    (set-window-start w1 s2)
+    (set-window-start w2 s1)))))
